0a3e5ebc357c1f306f8e36dd0a13bf289e8aa844
tarkan mert kazak <tarkanmertkazak@gmail.com>
2026-01-11 17:39:30 +0300

    firebase duzeltildi

--- firebase.js
+++ firebase.js
@@ -2,6 +2,7 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-analytics.js";
+  import { getFirestore } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,3 +21,4 @@
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
+  export const db = getFirestore(app);
